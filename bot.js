1f178851f24698f27c71872b69204a0bb251826f
ibrahim12651 <39805679+ibrahim12651@users.noreply.github.com>
2018-07-23 15:38:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -357,7 +357,7 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(ayarlar.token);
+client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
   client.setInterval(() => {
