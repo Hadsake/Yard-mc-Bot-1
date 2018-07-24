22f3abf00c79807aa6dce716ae3a7d65431707d2
ibrahim12651 <39805679+ibrahim12651@users.noreply.github.com>
2018-07-24 12:05:22 +0300

    Delete reklam_engelleme.js

--- komutlar/reklam_engelleme.js
+++
@@ -1,4 +0,0 @@
-client.on('message', msg => {
-  if (/(https?:\/\/)?(www\.)?(discord\.(gg|li|me|io)|discordapp\.com\/invite)\/.+/.test(msg.content)) return msg.delete()
-  .then(() => msg.reply('**Reklam Engellendi:shield:**'));
-  });
